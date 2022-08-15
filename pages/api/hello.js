dcb4281b277859e64c9ffedaa69be35a679a36d7
Alan Vasconcelos <alandbh@gmail.com>
2022-08-15 14:35:25 -0300

    Hotfix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,11 +1,11 @@
-const svg = `<svg viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
+const svg2 = `<svg viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
 <ellipse cx="100" cy="50" rx="100" ry="50" />
 </svg>`;
 
 export default function handler(req, res) {
     res.setHeader("content-type", "image/svg+xml");
 
-    res.status(200).send(svg);
+    res.status(200).send(svg2);
 
     // res.status(200).json({ name: "John Doe" });
 }
