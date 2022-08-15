80687f9e928017f38d966e6a2e1e2e7ed94a35e0
Alan Vasconcelos <alandbh@gmail.com>
2022-08-15 14:30:59 -0300

    Initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const svg = `<svg viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
+<ellipse cx="100" cy="50" rx="100" ry="50" />
+</svg>`;
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    res.setHeader("content-type", "image/svg+xml");
+
+    res.status(200).send(svg);
+
+    // res.status(200).json({ name: "John Doe" });
 }
